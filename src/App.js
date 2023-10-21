e7bca4d9eacc12cd0d2302e637020fb968a674d2
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-10-21 21:46:53 +0100

    Add files via upload
    Problem Description:
    The issue revolved around the inability to perform a new city search after returning to the WeatherForm component from WeatherDisplay. Initially, setting buttonActive to false in the WeatherForm component led to a situation where the component wouldn't render again.
    
    Solution:
    The solution involved setting buttonActive to false in the middle component, after WeatherForm was displayed and before WeatherDisplay was rendered. This approach allowed for a seamless transition between the components, enabling users to perform a new search for a different city without encountering rendering issues.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import './App.scss';
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from './routes/shop/shop.component';
+import Checkout from "./routes/checkout/checkout.component";
 
-import WeatherForm from './components/WeatherForm/weather-form.component';
-import WeatherDisplay from './components/WeatherDisplay/weather-display.component';
-
-import { useDataContext } from './DataContext/DataContext';
-import { useButtonContext } from './DataContext/ButtonContext';
-
-function App() {
-  const { weatherData } = useDataContext();
-  const { buttonActive } = useButtonContext();
-
-  console.log('Is HOME Button active?:', buttonActive)
-
+const App = () => {
   return (
-    <div className="App">
-      {buttonActive ? (<WeatherForm />) 
-        : weatherData ? (<WeatherDisplay />) : (<WeatherForm />)
-      }
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import './index.scss';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 
-import { DataProvider } from './DataContext/DataContext';
-import { ButtonProvider } from './DataContext/ButtonContext';
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { CategoriesProvider } from './contexts/categories.context';
+import { CartProvider } from './contexts/cart.context';
+
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ButtonProvider>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </ButtonProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
