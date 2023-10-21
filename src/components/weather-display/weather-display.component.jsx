ea6c11f8ea44516b2b2279f5046f5b34c2ca6fc4
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-10-21 21:56:16 +0100

    Delete src directory

--- src/components/weather-display/weather-display.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import './weather-display.styles.scss';
-
-const WeatherDisplay = () => {
-
-}
-
-export default WeatherDisplay;
--- src/components/weather-service/weather-service.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import './weather-service.styles';
-
-const WeatherService = () => {
-
-}
-
-export default WeatherService;
